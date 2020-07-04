4cd09ce228cc38960b395e176e6e232535db0772
vasily <artur.dvo@mail.ru>
2020-07-04 14:32:40 +0300

    update admin_site

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,55 @@
 from django.contrib import admin
+import datetime
+
 from .models import AdvUser
+from .utilities import send_activation_notification
+
+
 # Register your models here.
 
-admin.site.register(AdvUser)
+def send_activation_notifications(modeladmin, request, queryset):
+    """Отправка писем с оповещениями об активации"""
+    for rec in queryset:
+        if not rec.is_activated:
+            send_activation_notifications(rec)
+    modeladmin.message_user(request, 'Письма с оповещением отправлены')
+    send_activation_notifications.short_description = 'Отправка писем с оповещениями об активации'
+
+
+class NonActivatedFilter(admin.SimpleListFilter):
+    title = 'Прошли активацию?'
+    parameter_name = 'actstate'
+
+    def lookups(self, request, model_admin):
+        return {
+            ('activated', 'Прошли'),
+            ('threedays', 'Не прошли более 3-х дней'),
+            ('week', 'Не прошли более недели')
+        }
+
+    def queryset(self, request, queryset):
+        val = self.value()
+        if val == 'activated':
+            return queryset.filter(is_active=True, is_activated=True)
+        elif val == 'threedays':
+            d = datetime.date.today() - datetime.timedelta(days=3)
+            return queryset.filter(is_active=False, is_activated=False, date_joined__date__lt=d)
+        elif val == 'week':
+            d = datetime.date.today() - datetime.timedelta(weeks=1)
+            return queryset.filter(is_active=False, is_activated=False, date_joined__date__lt=d)
+
+
+class AdvUserAdmin(admin.ModelAdmin):
+    """Модель вывода инфы о пользователе в админке"""
+    list_display = ('__str__', 'is_activated', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = (NonActivatedFilter,)
+    fields = (('username', 'email'), ('first_name', 'last_name'),
+              ('send_messages', 'is_active', 'is_activated'),
+              ('is_staff', 'is_superuser'), ('groups', 'user_permissions'),
+              ('last_login', 'date_joined'))
+    readonly_fields = ('last_login', 'date_joined')
+    actions = (send_activation_notifications,)
+
+
+admin.site.register(AdvUser, AdvUserAdmin)
